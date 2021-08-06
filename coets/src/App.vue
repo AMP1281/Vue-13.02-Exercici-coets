a65793346ae1e60b3df8109e9f7271e2e9af0aba
Anna Montserrat <amp1281@gmail.com>
2021-08-06 13:22:13 +0200

    ok

--- coets/src/App.vue
+++ coets/src/App.vue
@@ -1,12 +1,32 @@
 <template>
   <div id="app">
-    <div id="nav">
+<!--     <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
-    </div>
+    </div> -->
     <router-view/>
+
   </div>
 </template>
+
+<script lang="ts">
+
+export default Vue.extend(options:{
+
+  components:{
+    Buttons
+  },
+  computed:{
+    helloMessage:{
+      get(): string {
+        return this.$store.state.user.email;
+      }
+    }
+  }
+});
+
+</script>
+
 
 <style>
 
